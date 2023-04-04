e0ec38766f9dd41958e8632af699287926233c63
David Herzog <david.herzog@engie.com>
2023-04-04 20:42:00 +0200

    added Formula Control calcChain to delete Formulas if needed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 import os
 
-VERSION = '0.2.2'
+VERSION = '0.2.3'
 DESCRIPTION = 'A package that allows to insert data in an XLSX file without changing the layout.'
 
 
